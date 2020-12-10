6407d7e21072c326d1235f652a8c88d3984a2c86
Deji <emmi2110@gmail.com>
2020-12-10 09:37:14 +0100

    new commit

--- longest_prefix.py
+++ longest_prefix.py
@@ -1,40 +1,19 @@
-def longestCommonPrefix(wordList):
-    if not wordList:
-        return ""
-    prefix = min(test, key=len)
-    for i in range(1, len(wordList)):
-        while(wordList[i].index(prefix) != 0):
-            prefix[:len(prefix) - 1]
-    return prefix
-
-test = ["flower", "flow", "flight"]
-# pre = min(test, key=len)
-# for i, c in enumerate(pre):
-#     for words in test:
-#         if word[i] != c:
-#             return pre[:i]
-
-# return(pre)
-
-# if not wordList:
-#         return ""
-#     prefix = min(test, key=len)
-#     for i, c in enumerate(prefix):
-#         for word in test:
-#             if word[i] != c:
-#                 return prefix[:i]
-#     return prefix    
-
-print(longestCommonPrefix(test))
-
-
-# word = ["flower", "flow", "flight"]
-# test = min(word, key=len)
-# for i in range(len(word)-1):
-#     print(word[i].index(test))
-
 #Algorithm
 #1 Set the min word in the list of words to be the prefix
 #2 loop through every word in the list of word and find the index of the prefix in that word,
 #using the wordList[word].index(prefix). if it's not 0, remove a letter from the back of the prefix and 
 #run the loop again, return prefix when done.
+
+def longestCommonPrefix(wordList):
+    if not wordList:
+        return "" ##return " " if input is not valid
+    prefix = min(wordList, key=len) ##get the smallest word in the input array
+    for i, c in enumerate(prefix):
+        for word in wordList:
+            if word[i] != c:
+                return prefix[:i]
+
+
+test = ["flower", "flow", "flight"]
+
+print(longestCommonPrefix(test))
